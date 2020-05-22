3497ecae3f76e21a9471bd8aef327a67ff7e8aeb
Eric K Wu <ekuan.wu@gmail.com>
2020-05-22 12:16:44 -0700

    Add dotenv-webpack plugin

--- packages/extension/webpack.common.js
+++ packages/extension/webpack.common.js
@@ -1,43 +1,45 @@
-const path = require("path");
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: {
-        contentPage: path.join(__dirname, "src/contentPage.tsx"),
-        backgroundPage: path.join(__dirname, "src/backgroundPage.ts"),
-        popup: path.join(__dirname, "src/popup/index.tsx"),
-    },
-    output: {
-        path: path.join(__dirname, "dist/js"),
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: "ts-loader",
-            },
-            {
-                exclude: /node_modules/,
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: "style-loader", // Creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader", // Translates CSS into CommonJS
-                    },
-                    {
-                        loader: "sass-loader", // Compiles Sass to CSS
-                    },
-                ],
-            },
+  entry: {
+    contentPage: path.join(__dirname, 'src/contentPage.tsx'),
+    backgroundPage: path.join(__dirname, 'src/backgroundPage.ts'),
+    popup: path.join(__dirname, 'src/popup/index.tsx'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader', // Creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // Translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader', // Compiles Sass to CSS
+          },
         ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src/'),
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {
-            "@src": path.resolve(__dirname, "src/"),
-        },
-    },
+  },
+  plugins: [new Dotenv()],
 };
