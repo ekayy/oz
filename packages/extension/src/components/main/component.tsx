ece659d1267729a80267016fd43ae16a3f001cad
Eric K Wu <ekuan.wu@gmail.com>
2020-05-27 18:26:01 -0700

    Reset state when fetching brands fails

--- packages/extension/src/components/main/component.tsx
+++ packages/extension/src/components/main/component.tsx
@@ -18,31 +18,39 @@ interface Brand {
 
 export const Main: React.FC<{ brandName: string }> = ({ brandName }) => {
   const [data, setData] = useState<Brand>({});
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchBrand = async (brand: string): Promise<void> => {
       try {
+        setIsFetching(true);
         const response = await axios.get(API_URL, {
           params: { name: brand },
         });
         setData(response.data);
+        setIsFetching(false);
       } catch (err) {
-        throw new Error(err);
         setData({});
+        setIsFetching(false);
+        throw new Error(err);
       }
     };
     fetchBrand(brandName);
   }, []);
 
   return (
-    <div className="container">
-      <img src={data.image} className="image" />
+    <>
+      {!isFetching && (
+        <div className="container">
+          <img src={data.image} className="image" />
 
-      <ul className="list">
-        <li>Parent Company: {data.parent}</li>
-        {data.payRatio && <li>Parent CEO Pay Ratio: {data.payRatio}:1</li>}
-        {data.taxRate && <li>Parent Effective Tax Rate: {(data.taxRate * 100).toFixed(2)}%</li>}
-      </ul>
-    </div>
+          {/* <ul className="list">
+            <li>Parent Company: {data.parent}</li>
+            {data.payRatio && <li>Parent CEO Pay Ratio: {data.payRatio}:1</li>}
+            {data.taxRate && <li>Parent Effective Tax Rate: {(data.taxRate * 100).toFixed(2)}%</li>}
+          </ul> */}
+        </div>
+      )}
+    </>
   );
 };
